dc39ab37754ec6d64523074d59c919b113f88c4c
hellojunho <jo62698476@gmail.com>
2023-12-03 21:24:05 +0900

    finished project

--- stocks/urls.py
+++ stocks/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('a', views.home, name='home'),
-    path('a', views.home, name='home'),
+    path('', views.home, name='home'),
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import requests
+import json
 
-# Create your views here.
+def home(reqeust):
+    try:
+        ticker = reqeust.GET['ticker']
+        stock_api = requests.get("https://api.iex.cloud/v1/data/core/quote/"+ticker+"?token=pk_0ebe670bfde7478984c98960631143b9")
+        stock = json.loads(stock_api.content)   # url에서 get 해온 json 데이터를 dict 형태로 저장
+    except Exception as e:
+        stock = ""
+        
+    content = {'stock':stock}
 
-def home(request):
-    # return HttpResponse("AAA") 
-    return render(request, 'stocks/home.html') 
+    return render(reqeust, 'stocks/home.html', content)
